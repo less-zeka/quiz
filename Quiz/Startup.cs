c2b6826a7ae3160021dbba5cbd25e92d7e709bdb
less <info@open-source.tips>
2016-03-26 22:11:35 +0100

    Initial commit

---
+++ Quiz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(Quiz.Startup))]
+namespace Quiz
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
